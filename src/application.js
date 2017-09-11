2b41eeb97adf16d7b9a79c8d63ab5d8bbab423a4
edapx <lastexxit@gmail.com>
2017-09-11 20:12:36 +0200

    wip zebraman

--- src/application.js
+++ src/application.js
@@ -11,70 +11,49 @@ const scene = new THREE.Scene();
 const OrbitControls = require('three-orbit-controls')(THREE);
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer({antialias:true});
-renderer.setSize(window.innerWidth, window.innerHeight);
-
-document.body.style.margin =0;
-document.body.appendChild(renderer.domElement);
-camera.position.z = 80;
-this.controls = new OrbitControls(camera, renderer.domElement);
-
-// stats
 const stats = new Stats();
-stats.showPanel( 0 ); // 0: fps, 1: ms, 2: mb, 3+: custom
-
-
-//scene
 const materials = new CollectionMaterials;
 const geometries = new CollectionGeometries;
-var objects = [];
-var group = new THREE.Group();
 
-//lights
-let ambientLight = new THREE.AmbientLight( 0x000000 );
-scene.add( ambientLight );
-gui.addScene(scene, ambientLight, renderer);
-gui.addMaterials(materials);
+function init(){
+    renderer.setSize(window.innerWidth, window.innerHeight);
 
-let lights = [];
-lights[ 0 ] = new THREE.PointLight( 0xffffff, 1, 0 );
-lights[ 1 ] = new THREE.PointLight( 0xffffff, 1, 0 );
-lights[ 2 ] = new THREE.PointLight( 0xffffff, 1, 0 );
+    document.body.style.margin =0;
+    document.body.appendChild(renderer.domElement);
+    camera.position.z = 80;
+    this.controls = new OrbitControls(camera, renderer.domElement);
 
-lights[ 0 ].position.set( 0, 200, 0 );
-lights[ 1 ].position.set( 100, 200, 100 );
-lights[ 2 ].position.set( - 100, - 200, - 100 );
+    // stats
+    stats.showPanel( 0 ); // 0: fps, 1: ms, 2: mb, 3+: custom
 
-scene.add( lights[ 0 ] );
-scene.add( lights[ 1 ] );
-scene.add( lights[ 2 ] );
+    //lights
+    let ambientLight = new THREE.AmbientLight( 0x000000 );
+    scene.add( ambientLight );
+    gui.addScene(scene, ambientLight, renderer);
+    gui.addMaterials(materials);
 
+    let light = new THREE.PointLight( 0xffffff, 1, 0 );
+    light.position.set( 0, 200, 0 );
+    scene.add( light );
 
-var axisHelper = new THREE.AxisHelper( 50 );
-//scene.add( axisHelper );
+    var axisHelper = new THREE.AxisHelper( 50 );
+    //scene.add( axisHelper );
 
-window.addEventListener('resize', function() {
-    var WIDTH = window.innerWidth,
-    HEIGHT = window.innerHeight;
-    renderer.setSize(WIDTH, HEIGHT);
-    camera.aspect = WIDTH / HEIGHT;
-    camera.updateProjectionMatrix();
-});
+    window.addEventListener('resize', function() {
+        var WIDTH = window.innerWidth,
+        HEIGHT = window.innerHeight;
+        renderer.setSize(WIDTH, HEIGHT);
+        camera.aspect = WIDTH / HEIGHT;
+        camera.updateProjectionMatrix();
+    });
 
-addStats(debug);
-render();
+    let object = new THREE.Mesh(geometries["box"], materials["phong"]);
+    scene.add(object);
 
-function populateGroup(selected_geometry, selected_material) {
-    for (var i = 0; i< gui.params.num; i++) {
-        let coord = {x:i, y:i, z:i};
-        let object = new THREE.Mesh(selected_geometry, selected_material);
-        object.position.set(coord.x, coord.y, coord.z);
-        object.rotateY( (90 + 40 + i * 100/gui.params.num ) * -Math.PI/180.0 );
-
-        objects.push(object);
-        group.add(object);
-    }
-    scene.add(group);
+    addStats(debug);
+    render();
 }
+
 
 function addStats(debug) {
     if (debug) {
@@ -83,25 +62,15 @@ function addStats(debug) {
 }
 
 
-function resetGroup(){
-    for(var index in objects){
-        let object = objects[index];
-	    group.remove( object );
-    }
-    scene.remove(group);
-    objects = [];
-}
 
 function render(){
     stats.begin();
-    populateGroup(geometries[gui.params.geometry],materials[gui.params.material]);
-    if(gui.params.rotate_flower){
-        group.rotateZ( 0.0137);
-    }
-	renderer.render(scene, camera);
-    resetGroup();
+    renderer.render(scene, camera);
     stats.end();
-	requestAnimationFrame(render);
+    requestAnimationFrame(render);
 }
+
+
+init();
 
 
