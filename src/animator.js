2b44006fe62b7f1cdb8cf296d70a821a277b23d7
edapx <lastexxit@gmail.com>
2017-09-23 17:17:52 +0200

    refactor

--- src/animator.js
+++ src/animator.js
@@ -7,66 +7,66 @@ let flying = false;
 export default class Animator {
     constructor(){
         this.spline = this._createCurve();
-        this.positionOnTheCurve = 0; //value that goes from 0 to 1
         this.speed = 0.0008;
-        this.schedule = {x: 0, y:0};
+        this.schedule = {x: 0, y:-1};
         this.destination = {x:1.0, y:1.0};
-        this.seconds = 10;
+
         // Fly animation
         this.timeNextFly = 0;
         this.flyDurationMill = 9000;
+        this.calmStartSec = 2; // at the beginning do not flip
         this.minIntervalMill = this.flyDurationMill + 500;
         this.maxIntervalMill = this.minIntervalMill + 5000;
     }
 
-    getRandomArbitrary(min, max){
-        return Math.random() * (max -min) +min;
+    _getRandomDelay(){
+        return Math.random() * (this.maxIntervalMill -this.minIntervalMill) + this.minIntervalMill;
     }
 
     move(time, objects, group){
         TWEEN.update();
-        if (time >= this.timeNextFly && flying === false) {
+        if (time >= this.timeNextFly && time > this.calmStartSec && flying === false) {
             this.schedule = {x:0, y:0};
-            let delay = Math.random() * (this.maxIntervalMill -this.minIntervalMill) + this.minIntervalMill;
-            this.flyAround = new TWEEN.Tween(this.schedule) // Create a new tween that modifies 'coords'.
-                .to(Object.assign({}, this.destination), this.flyDurationMill) // Move to (300, 200) in 1 second.
-                .easing(TWEEN.Easing.Quadratic.Out) // Use an easing function to make the animation smooth.
-                .onUpdate( (current) =>{ // Called after tween.js updates 'coords'.
-                    // Move 'box' to the position described by 'coords' with a CSS translation.
-                    this._moveInCircle(time, current, group);
-                    this._flip(objects, time);
-                }).onComplete(() => {flying = false;})
+            let delay = this._getRandomDelay();
+            this.flyAround = new TWEEN.Tween(this.schedule)
+                .to(Object.assign({}, this.destination), this.flyDurationMill)
+                .easing(TWEEN.Easing.Quadratic.Out)
+                .onUpdate( (current) =>{
+                    this._fly(objects, current, group );
+                }).onComplete( () => {flying = false;} )
             .start().delay(delay);
             flying = true;
             this.timeNextFly = time + (this.flyDurationMill + delay) / 1000.0;
         }
-
-        //this._moveInCircle(time, this.schedule, group);
-        //start with a circular movement, and an animation dictated from a value that goes from 0 to 1
     }
 
-    _flip(objects, time){
+    _fly(objects,current, group ){
+        this._moveInCircle(current, group);
+        //this._flip(objects, current.x);
+        this._flip(objects, current);
+    }
+
+    _flip(objects, current){
+        let angle = Math.sin(current.y * 12) * 0.04 ;
+        console.log(angle);
         for (var index in objects) {
             let object = objects[index];
-            object.rotateOnAxis(new Vector3(0, 0 ,1), Math.sin(time) * 0.2);
+            object.rotateOnAxis(new Vector3(0, 0 ,1), angle);
         }
     }
 
-    _moveInCircle(time, currFrame, group, n_circles = 1){
+    _moveInCircle(currFrame, group, n_circles = 1){
         let currPos = this.spline.getPointAt(currFrame.x);
-        console.log(currPos);
-        console.log(currFrame.x);
-
         group.position.set(currPos.x, 0, currPos.z);
-        let next = this.positionOnTheCurve + this.speed * 20;
+
+        //orientate to next point on curve direction
+        let next = currFrame.x + this.speed * 20;
         let lookAtPoint = (next > 1) ? 0 : next;
         let look = this.spline.getPointAt(lookAtPoint);
-        //orientate to next point on curve direction
         group.lookAt(look);
+
         //but keep the crown looking up
         group.rotateX(-Math.PI/2);
-        let limit = 1 - this.speed;
-        this.positionOnTheCurve= (this.positionOnTheCurve>= limit) ? 0 : this.positionOnTheCurve+= this.speed;
     }
 
     addDebugCurve(scene){
