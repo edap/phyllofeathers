45f550e8d1a27c62c8c42e93fbd097bd6ed6ecc0
edapx <lastexxit@gmail.com>
2017-10-03 17:33:38 +0200

    fix webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const PUBLIC = __dirname + "/www";
+const webpack = require('webpack');
 const TITLE = "Example Title";
 
 
@@ -15,43 +16,50 @@ const plugins = [
 			template: 'src/index.html'
 		}),
 		//copy the assets (with no css compilation)
-        new CopyWebpackPlugin([
-            {from:'src/textures',to:'textures'},
-            {from:'src/css',to:'css'},
-            {from:'src/sounds', to: 'sounds'}
-        ]),
-        // clean the output folder
-        new CleanWebpackPlugin(['www']),
+    new CopyWebpackPlugin([
+        {from:'src/textures',to:'textures'},
+        {from:'src/css',to:'css'},
+        {from:'src/sounds', to: 'sounds'}
+    ]),
+    // clean the output folder
+    new CleanWebpackPlugin(['www']),
+    new webpack.optimize.UglifyJsPlugin({
+			  include: /\.min\.js$/,
+			  sourceMap: true,
+			  compressor: {
+				    warnings: false
+			  }
+		})
 ];
 
 module.exports = {
-	target: 'web',
-	devtool: 'source-map',
-	entry: './src/application.js',
-	output: {
-		path: PUBLIC,
-		filename: 'bundle.js',
-		publicPath: ''
-	},
-	plugins: plugins,
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				include: [
-					path.resolve(__dirname, 'src')
-				],
-				loader: 'babel-loader',
-				query: {
-					compact: true,
-					presets: [
-						['es2015', {modules: false}]
-					]
-				}
-			}
-		]
-	},
-	performance: {
-		hints: false
-	}
+    target: 'web',
+    devtool: 'source-map',
+    entry: {
+        "bundle": "./src/application.js",
+        "bundle.min": "./src/application.js"
+    },
+
+    output: {
+        filename: "[name].js",
+        path: PUBLIC
+    },
+    plugins: plugins,
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                include: /src/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["env"]
+                    }
+                }
+            }
+        ]
+    },
+    performance: {
+    hints: false
+    }
 };
