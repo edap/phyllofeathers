1cb7945fad7fc141a8287381ece05e195f437e12
edapx <lastexxit@gmail.com>
2017-10-29 14:56:50 +0100

    clean up old buffers initialization

--- src/application.js
+++ src/application.js
@@ -21,29 +21,21 @@ import Flower from './flower.js';
 import { limitControls } from './utils.js';
 import { getPlaneShader } from './shaders.js';
 import Scenographer from './scenographer.js';
+import BufferManager from './buffersManager.js';
+//setup scene and camera
 const scene = new THREE.Scene();
 let scenographer;
 const OrbitControls = require('three-orbit-controls')(THREE);
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 2000);
 const renderer = new THREE.WebGLRenderer({antialias:true, transparent:true});
-const targetSize = 4096;
 const animator = new Animator();
-
 renderer.setSize(window.innerWidth, window.innerHeight);
 const maxAnisotropy = renderer.capabilities.getMaxAnisotropy();
 
 //BufferScene
+const targetSize = 4096;
+let buffers;
 let trailsOn = true;
-let bufferScene;
-let textureA;
-let textureB;
-let bufferMaterial;
-let plane;
-let bufferObject;
-let finalMaterial;
-let slideDirection = new THREE.Vector2(1.0, 1.0);
-let quad;
-let fragShader = getPlaneShader();
 
 const clock = new THREE.Clock();
 const stats = new Stats();
@@ -69,8 +61,8 @@ function init(assets){
         camera.updateProjectionMatrix();
     });
 
-    buffer_texture_setup();
-    scenographer = new Scenographer(scene, bufferScene);
+    buffers = new BufferManager(targetSize);
+    scenographer = new Scenographer(scene, buffers.bufferScene);
     // ambient lights. TODO, use them or not?
     //let ambientLight = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
     //let ambientLight2 = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
@@ -78,7 +70,7 @@ function init(assets){
     //bufferScene.add(ambientLight);
     scenographer.turnLightOn();
 
-    if(debug){
+    if (debug) {
         gui = new Gui(regenerate, materials, assets.textures, maxAnisotropy, ParrotType, debug, wrongPhyllo);
         flower = new Flower(gui.params, materials, assets, ParrotType, maxAnisotropy);
         flower.makePetalsVisible(1.0);
@@ -104,7 +96,7 @@ function init(assets){
     limitControls(controls);
 
     document.body.addEventListener("keypress", maybeSpacebarPressed);
-    animator.init(flower, quad, slideDirection);
+    animator.init(flower, buffers.quad, buffers.slideDirection);
     render();
 }
 
@@ -115,15 +107,8 @@ function render(){
     if (!debug) animator.update();
 
     if (trailsOn) {
-        var t = textureA;
-        textureA = textureB;
-        textureB = t;
-        quad.material.map = textureB.texture;
-        //quad.rotateY(0.005);
-        bufferMaterial.uniforms.bufferTexture.value = textureA.texture;
-        bufferMaterial.uniforms.slideDirection.value = slideDirection;
-        // draw to texture B
-        renderer.render(bufferScene, camera, textureB, true);
+        buffers.update();
+        renderer.render(buffers.bufferScene, camera, buffers.textureB, true);
     }
     renderer.render(scene, camera);
     stats.end();
@@ -144,39 +129,9 @@ function toggleTrails(){
     } else {
         scenographer.removeFromSceneByName('flower');
         scenographer.addToBufferScene(flower.group);
-        scenographer.add(quad);
+        scenographer.add(buffers.quad);
         trailsOn = true;
     }
-}
-
-function buffer_texture_setup(){
-    //Create buffer scene
-    bufferScene = new THREE.Scene();
-    //Create 2 buffer textures
-    textureA = new THREE.WebGLRenderTarget( targetSize, targetSize, { minFilter: THREE.LinearFilter, magFilter: THREE.NearestFilter});
-    textureB = new THREE.WebGLRenderTarget(targetSize, targetSize, { minFilter: THREE.LinearFilter, magFilter: THREE.NearestFilter} );
-    //Pass textureA to shader
-    slideDirection = new THREE.Vector2(1.0,1.0);
-    bufferMaterial = new THREE.ShaderMaterial( {
-        uniforms: {
-            bufferTexture: { type: "t", value: textureA.texture },
-            slideDirection: {type: 'v2', value: slideDirection},
-            res : {type: 'v2',value:new THREE.Vector2(targetSize, targetSize)}//Keeps the resolution
-        },
-        fragmentShader: fragShader
-    } );
-    var plane = new THREE.PlaneBufferGeometry( window.innerWidth, window.innerHeight );
-    bufferObject = new THREE.Mesh( plane, bufferMaterial );
-    bufferScene.add(bufferObject);
-
-    //Draw textureB to screen
-    //finalMaterial =  new THREE.MeshBasicMaterial({map: textureB.texture, color:0XFF0000});
-    finalMaterial =  new THREE.MeshBasicMaterial({map: textureB.texture, transparent:true});
-    finalMaterial.side = THREE.DoubleSide; //just in case you are rotating the plane
-    quad = new THREE.Mesh( plane, finalMaterial );
-    quad.name = 'quad';
-    //quad.rotateY(Math.PI/2);
-    //scene.add(quad);
 }
 
 let regenerate = () => {
