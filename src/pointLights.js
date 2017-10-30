0f2e5607256f16c4a5c601bf559858f6cfbcc6ce
edapx <lastexxit@gmail.com>
2017-10-30 16:21:10 +0100

    fix point lights

--- src/pointLights.js
+++ src/pointLights.js
@@ -1,14 +1,14 @@
-import {PointLight} from 'three';
+import { PointLight } from 'three';
 
-export function PointLights(distanceFromCenter = 10, intensity = 1) {
-    // it positions 3 Point Lights on tree points, equidistant from the center
-    let lights = [];
-    lights[ 0 ] = new PointLight( 0xffffff, intensity, 0 );
-    lights[ 1 ] = new PointLight( 0xffffff, intensity, 0 );
-    lights[ 2 ] = new PointLight( 0xffffff, intensity, 0 );
+export function PointLights(distanceFromCenter = 10, intensity = 1){
+	// it positions 3 Point Lights on tree points, equidistant from the center
+	const lights = [];
+	lights[0] = new PointLight(0xffffff, intensity, 0);
+	lights[1] = new PointLight(0xffffff, intensity, 0);
+	lights[2] = new PointLight(0xffffff, intensity, 0);
 
-    lights[ 0 ].position.set( 0, distanceFromCenter, 0 );
-    lights[ 1 ].position.set( distanceFromCenter, distanceFromCenter, distanceFromCenter );
-    lights[ 2 ].position.set( distanceFromCenter, distanceFromCenter, distanceFromCenter );
-    return lights;
+	lights[0].position.set(0, distanceFromCenter, 0);
+	lights[1].position.set(distanceFromCenter, 0, distanceFromCenter);
+	lights[2].position.set(0, distanceFromCenter, distanceFromCenter);
+	return lights;
 }
