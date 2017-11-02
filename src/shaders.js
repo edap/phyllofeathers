c97cfd93726090e63b5b3f6bccfb6ea76f624688
edapx <lastexxit@gmail.com>
2017-11-02 22:30:50 +0100

    disable slideDirection temporaly

--- src/shaders.js
+++ src/shaders.js
@@ -1,14 +1,15 @@
-let glsl = require('glslify');
+const glsl = require('glslify');
 
 export function getPlaneShader(){
-    return glsl`
+	return glsl`
 		uniform vec2 res;//The width and height of our screen
 		uniform vec2 slideDirection;// in which direction the texture will slide
 		uniform sampler2D bufferTexture;//Our input texture
 		void main() {
 			vec2 st = gl_FragCoord.xy / res;
 			vec2 uv = st;
-			uv *= slideDirection;
+			//uv *= slideDirection;
+			uv*= 1.02;
 			gl_FragColor = texture2D(bufferTexture, uv);
     }`;
 }
