9fecacbc21ba2c002eeb4a65cd47bd824a874ff6
Davide Prati <lastexxit@gmail.com>
2017-11-12 17:22:43 +0100

    limit angle

--- src/utils.js
+++ src/utils.js
@@ -6,9 +6,10 @@ export function removeEntityByName(name, scene){
 }
 
 export function limitControls(controls){
-	controls.minPolarAngle = Math.PI / 6.5;
-	controls.maxPolarAngle = Math.PI / 1.1;
-	controls.minDistance = 100; //era 100
-	//controls.minDistance = 20;
+	controls.minPolarAngle = Math.PI / 4.5;
+	controls.maxPolarAngle = Math.PI / 1.4;
+	controls.minAzimuthAngle = -Math.PI / 4;
+	controls.maxAzimuthAngle = Math.PI / 4;
+	controls.minDistance = 100;
 	controls.maxDistance = 200;
 }
